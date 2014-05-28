91a26828ec71651969cf6d2dd8513928bee85caf
redv1981 <redv1981@gmail.com>
2014-05-28 11:48:23 +0800

    调整JS调用

--- www/CapturePhotoVideo.js
+++ www/CapturePhotoVideo.js
@@ -2,12 +2,17 @@ var exec = require('cordova/exec');
 
 module.exports = {
 
-	capturePhoto: function(successCallback, errorCallback) {
-		exec(successCallback, errorCallback, 'CapturePhotoVideo', 'capturePhoto', []);
+	capturePhoto: function(callback, option) {
+		if (!option) option = {};
+		if (!option.width) option.width = 1280;
+		if (!option.height) option.height = 800;
+		if (!option.quality) option.quality = 75;
+
+		exec(callback, null, 'CapturePhotoVideo', 'capturePhoto', [option.width, option.height, option.quality]);
 	},
 
-	captureVideo: function(successCallback, errorCallback) {
-		exec(successCallback, errorCallback, 'CapturePhotoVideo', 'captureVideo', []);
+	captureVideo: function(callback) {
+		exec(callback, null, 'CapturePhotoVideo', 'captureVideo', []);
 	}
 
 };
