c5c395af2d62aa9bb161860b7a8e112ec3b12790
redv1981 <redv1981@gmail.com>
2014-05-29 12:18:47 +0800

    增加把文件编码为Base64字符串的方法

--- src/android/CapturePhotoVideo.java
+++ src/android/CapturePhotoVideo.java
@@ -1,10 +1,14 @@
 package cordova.plugins.capturephotovideo;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
 
 import android.app.Activity;
 import android.content.Intent;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
@@ -12,104 +16,119 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class CapturePhotoVideo extends CordovaPlugin {
-	public static final String TAG = "CapturePhotoVideo";
+    public static final String TAG = "CapturePhotoVideo";
 
-	public static final String CLASS_PHOTO = "cordova.plugins.capturephotovideo.CapturePhotoActivity";
-	public static final String CLASS_VIDEO = "cordova.plugins.capturephotovideo.CaptureVideoActivity";
+    public static final String CLASS_PHOTO = "cordova.plugins.capturephotovideo.CapturePhotoActivity";
+    public static final String CLASS_VIDEO = "cordova.plugins.capturephotovideo.CaptureVideoActivity";
 
-	public static final int APTURE_VIDEO = 200;
-	public static final int CAMERA_PHOTO = 100;
+    public static final int APTURE_VIDEO = 200;
+    public static final int CAMERA_PHOTO = 100;
 
-	public static int CompressWidth = 1280;
-	public static int CompressHeight = 800;
-	public static int CompressOption = 75;
+    public static int CompressWidth = 1280;
+    public static int CompressHeight = 800;
+    public static int CompressOption = 75;
 
-	private CallbackContext callbackContext;
+    private CallbackContext callbackContext;
 
-	public CapturePhotoVideo() {
+    public CapturePhotoVideo() {
 
-	}
+    }
 
-	@Override
-	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		this.callbackContext = callbackContext;
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        this.callbackContext = callbackContext;
 
-		if (action.equals("capturePhoto")) {
-			if (null != args && 0 < args.length()) {
-				if (0 < args.getInt(0)) {
-					CompressWidth = args.getInt(0);
-				}
-				if (0 < args.getInt(1)) {
-					CompressHeight = args.getInt(1);
-				}
-				if (0 < args.getInt(2) && 100 >= args.getInt(2)) {
-					CompressOption = args.getInt(2);
-				}
-			}
+        if (action.equals("capturePhoto")) {
+            if (null != args && 0 < args.length()) {
+                if (0 < args.getInt(0)) {
+                    CompressWidth = args.getInt(0);
+                }
+                if (0 < args.getInt(1)) {
+                    CompressHeight = args.getInt(1);
+                }
+                if (0 < args.getInt(2) && 100 >= args.getInt(2)) {
+                    CompressOption = args.getInt(2);
+                }
+            }
 
-			return capturePhoto();
-		} else if (action.equals("captureVideo")) {
-			return captureVideo();
-		} else {
-			return false;
-		}
-	}
+            return capturePhoto();
+        } else if (action.equals("captureVideo")) {
+            return captureVideo();
+        } else {
+            return false;
+        }
+    }
 
-	private boolean capturePhoto() {
-		if (null != callbackContext) {
-			Intent intent = new Intent()
-			.putExtra("CompressWidth", CompressWidth)
-			.putExtra("CompressHeight", CompressHeight)
-			.putExtra("CompressOption", CompressOption)
-			.setClassName(cordova.getActivity(), CLASS);
-			cordova.startActivityForResult(this, intent, CAMERA_PHOTO);
-			return true;
-		} else {
-			return false;
-		}
-	}
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-	private boolean captureVideo() {
-		if (null != callbackContext) {
-			Intent intent = new Intent()
-			.setClassName(cordova.getActivity(), CLASS_VIDEO);
-			cordova.startActivityForResult(this, intent, APTURE_VIDEO);
-			return true;
-		} else {
-			return false;
-		}
-	}
+        if (Activity.RESULT_OK == resultCode) {
+            switch (requestCode) {
+            case CAMERA_PHOTO:
+                List<String> json = data.getStringArrayListExtra("list");
 
-	public void failPicture(String err) {
-		this.callbackContext.error(err);
-	}
+                System.out.print(json);
 
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
+                JSONArray path = new JSONArray();
 
-		if (Activity.RESULT_OK == resultCode) {
-			switch (requestCode) {
-			case CAMERA_PHOTO:
-				List<String> json = data.getStringArrayListExtra("list");
+                for (int i = 0, c = json.size(); c > i; i++) {
+                    JSONObject j = JsonUtil.getJson(json.get(i));
+                    path.put(j);
+                }
 
-				System.out.print(json);
+                callbackContext.success(path);
+                break;
 
-				JSONArray path = new JSONArray();
+            case APTURE_VIDEO:
+                break;
+            }
+        } else if (Activity.RESULT_CANCELED == resultCode) {
+            this.failPicture("Camera cancelled.");
+        }
+    }
 
-				for (int i = 0, c = json.size(); c > i; i++) {
-					JSONObject j = JsonUtil.getJson(json.get(i));
-					path.put(j);
-				}
+    private boolean capturePhoto() {
+        if (null != callbackContext) {
+            Intent intent = new Intent()
+            .putExtra("CompressWidth", CompressWidth)
+            .putExtra("CompressHeight", CompressHeight)
+            .putExtra("CompressOption", CompressOption)
+            .setClassName(cordova.getActivity(), CLASS);
+            cordova.startActivityForResult(this, intent, CAMERA_PHOTO);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-				callbackContext.success(path);
-				break;
+    private boolean captureVideo() {
+        if (null != callbackContext) {
+            Intent intent = new Intent()
+            .setClassName(cordova.getActivity(), CLASS_VIDEO);
+            cordova.startActivityForResult(this, intent, APTURE_VIDEO);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-			case APTURE_VIDEO:
-				break;
-			}
-		} else if (Activity.RESULT_CANCELED == resultCode) {
-			this.failPicture("Camera cancelled.");
-		}
-	}
+    private String encodeFileToBase64String(String filename) throws IOException {
+        File file = new File(filename);
+
+        try (FileInputStream stream = new FileInputStream(file)) {
+            byte[] bytes = new byte[(int)file.length];
+
+            if (bytes.length == stream.read(bytes, 0, bytes.length)) {
+                byte[] encoded = Base64.encodeBase64(bytes);
+                String encodedString = new String(encoded);
+
+                return encodedString;
+            }
+        }
+    }
+
+    private void failPicture(String err) {
+        this.callbackContext.error(err);
+    }
 }
